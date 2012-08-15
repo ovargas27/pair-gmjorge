2b42eff27e69200b0dd8d3b1cb20095bb9e16653
Jorge García <jorge.em223@gmail.com>
2012-08-15 09:50:27 -0500

    Scheme updated
    Is necessary upload the schema?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815042206) do
+ActiveRecord::Schema.define(:version => 20120815140703) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(:version => 20120815042206) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "status"
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], :name => "index_books_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "provider"
