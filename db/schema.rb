075455c32e899347005d9d33c69914642f4c7298
Jorge García <jorge.em223@gmail.com>
2012-08-15 00:04:26 -0500

    Add schema
    I think don't need to upload to git

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815020727) do
+ActiveRecord::Schema.define(:version => 20120815042206) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
     t.string   "ISBN"
     t.string   "author"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "status"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
