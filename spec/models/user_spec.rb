f632d9bd8308d11041474fa84537ba8271e4e75b
Jorge García <jorge.em223@gmail.com>
2012-08-14 23:40:33 -0500

    Fix the user test with correct syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  it "should be created a valid user" do
+  it "should be saved with a valid attributes" do
     FactoryGirl.create(:user).should be_valid
   end
 
-  it "should not be create a invalid user" do
+  it "should not be saved with invalid attributes" do
     expect { FactoryGirl.create(:user, uid: '') }.to raise_error
   end
 end
